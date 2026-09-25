d82d80369235bbf1a2f431763f6ca01e4defbc4d
Moeis Azam Butt <mab10269@nyu.edu>
2026-09-25 16:25:43 -0400

    Fix syntax errors in smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -27,7 +27,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     
     clientsocket.send(msg.encode())
     
-    clientsocket.send(endmsg.())
+    clientsocket.send(endmsg.encode())
     recv5 = clientsocket.recv(1024).decode()
     
     quitcommand = "quit\r\n"
